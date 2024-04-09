d2b0329e143355bc000fb2d50a6b6b17c070f511
Thomas <nyumat18@gmail.com>
2024-04-08 18:25:11 -0700

    boilerplate: added initial blueprint chi + mongo

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"damstudy/internal/server"
+	"fmt"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
